727c3cad6ba9b34b637170ccc6bfef5d872322ac
Robert Schuster <schuster@meteo.uni-koeln.de>
2013-05-22 20:38:34 +0200

    worked on hash map

--- src/mod_map.f90
+++ src/mod_map.f90
@@ -4,19 +4,29 @@ module mod_map
 
     ! the map itself
     type map
-        class(node), dimension(:), pointer :: table
+        class(nodepointer), dimension(:), pointer :: table
         integer :: length
         integer :: initialSize
         contains
         procedure add                       ! add an element to the new map
         procedure positionForHash           ! calculate the index of the hash key in the table
+        procedure printContent              ! print the table structure for debuging
+        ! deallocate all elements
+        procedure removeAll
     end type
     ! define the constructor for the map
     interface map
         module procedure constructor_map
     end interface
 
-    ! the hash table consists of an arrayof nodes where each node can have children
+    ! the hash table itself consists of an array of nodepointers where each can point to an node object
+    ! the object holds also the number of nodes
+    type nodepointer
+        class(node), pointer :: thenode
+        integer :: length
+    end type
+
+    ! the hash table is filled with nodes where each node can have children
     type node
         class(node), pointer :: next
         ! the key used to insert the value
@@ -27,6 +37,8 @@ module mod_map
         integer (kind=8) :: hash
         ! is the value a copy? the key is always a copy
         logical :: valueIsCopy
+        contains
+        procedure release                   ! deallocate the memory of this node and all children
     end type
     ! define the constructor for the node
     interface node
@@ -40,10 +52,10 @@ contains
     ! a very simple hash function, see sdbm on http://www.cse.yorku.ca/~oz/hash.html
     function calculateHash(key)
         integer (kind=8) :: calculateHash
-        class(*) :: key
+        class(*), intent(in) :: key
 
         !local variables for character key
-        integer :: length, i
+        integer :: length
         integer (kind=1), dimension(:), allocatable :: chars
 
         select type (key)
@@ -54,17 +66,33 @@ contains
                 ! transfer the contents of the string to an integer array
                 allocate(chars(length))
                 chars = transfer(key, chars)
-                do i = 1, length
-                    calculateHash = chars(i) + shiftl(calculateHash, 6) + shiftl(calculateHash, 16) - calculateHash
-                end do
+                calculateHash = calculateSDBMhash(chars)
                 ! free the memory
                 deallocate(chars)
             type is (real (kind=8))
-                calculateHash = transfer(key, calculateHash)
+                ! use sdbm
+                calculateHash = 0
+                ! transfer the contents of the real to an integer array
+                allocate(chars(8))
+                chars = transfer(key, chars)
+                calculateHash = calculateSDBMhash(chars)
+                ! free the memory
+                deallocate(chars)
             class default
                 ! not yet implemented for other types
                 call exit(1)
         end select
+    end function
+
+    ! calculate sdbm hash function from a byte array
+    function calculateSDBMhash(intarray)
+        integer (kind=8) :: calculateSDBMhash
+        integer (kind=1), dimension(:), intent(in) :: intarray
+        integer :: length, i
+        length = size(intarray)
+        do i = 1, length
+            calculateSDBMhash = intarray(i) + shiftl(calculateSDBMhash, 6) + shiftl(calculateSDBMhash, 16) - calculateSDBMhash
+        end do
     end function
 
     ! procedures of the map ---------------------------------------------------
@@ -72,7 +100,7 @@ contains
     ! the constructor of the map
     function constructor_map(isize)
         class(map), pointer :: constructor_map
-        integer, optional :: isize
+        integer, intent(in), optional :: isize
         ! allocate the memory for the new map
         allocate(constructor_map)
         constructor_map%table => null()
@@ -82,19 +110,21 @@ contains
         else
             constructor_map%initialSize = 10000
         end if
+        constructor_map%length = 0
     end function
 
     ! add an element to the map
     subroutine add(this, key, value, copy)
         class(map) :: this
-        class(*), target :: key
+        class(*), intent(in) :: key
         class(*), target :: value
         logical, optional :: copy
         logical :: copyValue
 
         ! local variables
-        integer :: pos
-        class(node), pointer :: newnode
+        integer :: pos = 0
+        class(node), pointer :: newnode, currentnode, lastnode
+        logical :: found
 
         ! copy or link?
         if (present(copy)) then
@@ -106,6 +136,10 @@ contains
         ! create the root if not already present
         if (.not.associated(this%table)) then
             allocate(this%table(this%initialSize))
+            do pos = 1, this%initialSize
+                this%table(pos)%thenode => null()
+                this%table(pos)%length = 0
+            end do
         end if
 
         ! create a new node
@@ -114,27 +148,120 @@ contains
         ! calculate the position in the table
         pos = this%positionForHash(newnode%hash)
 
-        ! count this element
+        ! place the new node into the table
+        ! the first posibility is an empty cell of the table
+        if (this%table(pos)%length == 0) then
+            this%table(pos)%thenode => newnode
+            this%table(pos)%length = 1
+            ! count this element
+            this%length = this%length + 1
+        else
+            ! the second posibility is that a node is already present at this position.
+            ! has one of the existing nodes the same hash code? if so, replace it
+            currentnode => this%table(pos)%thenode
+            lastnode => null()
+            found = .false.
+            do
+                ! the node found in the table has the same hash code, replace it
+                if (currentnode%hash == newnode%hash) then
+                    ! it is the first node, replace it
+                    if (.not.associated(lastnode) .and. currentnode%hash == this%table(pos)%thenode%hash) then
+                        this%table(pos)%thenode => newnode
+                        newnode%next => currentnode%next
+                    else
+                        ! it is not the first node
+                        lastnode%next => newnode
+                        newnode%next => currentnode%next
+                    end if
+                    ! free the memory of the current node
+                    currentnode%next => null()
+                    call currentnode%release()
+                    deallocate(currentnode)
+                    ! leave the loop
+                    found = .true.
+                    exit
+                end if
+                ! leave the loop if no more nodes are present
+                if (.not.associated(currentnode%next)) exit
+                lastnode => currentnode
+                currentnode => currentnode%next
+            end do
+            ! if the key was not found, append it to the last node
+            if (found .eqv. .false.) then
+                currentnode%next => newnode
+                this%table(pos)%length = this%table(pos)%length + 1
+                ! count this element
+                this%length = this%length + 1
+            end if
+        end if
+
     end subroutine
 
     ! calculate the index within the hash table for a given hash code
     function positionForHash(this, hash)
         class(map) :: this
-        integer (kind=8) :: hash
+        integer (kind=8), intent(in) :: hash
         integer :: positionForHash
         ! calculate the modulo of the hash code
-        positionForHash = modulo(hash, this%initialSize)
+        positionForHash = modulo(hash, this%initialSize) + 1
     end function
+
+    ! print the table structure for debuging
+    subroutine printContent(this)
+        class(map) :: this
+        integer :: i
+        class(node), pointer :: currentnode
+        print*, ""
+        ! is the table present?
+        if (associated(this%table)) then
+            ! print the number of elements in the map
+            write (*, "(A,I8)") "Elements in the map:", this%length
+            ! loop over all table cells
+            do i = 1, this%initialSize
+                if (associated(this%table(i)%thenode)) then
+                    currentnode => this%table(i)%thenode
+                    write (*, "(A,I8,A,I20,A)") "cell:", i, " => node(", currentnode%hash, ")"
+                    do while (associated(currentnode%next))
+                        currentnode => currentnode%next
+                        write (*, "(A,I20,A)") "              => node(", currentnode%hash, ")"
+                    end do
+                else
+                    write (*, "(A,I8,A)") "cell:", i, " empty"
+                end if
+            end do
+        else
+            print*, "the map is completely empty"
+        end if
+        print*, ""
+    end subroutine
+
+    ! finalize the list, clean up the memory
+    subroutine removeAll(this)
+        class(map) :: this
+        integer :: i
+        ! loop over the complete table
+        do i = 1, this%initialSize
+            ! is here an element?
+            if (associated(this%table(i)%thenode)) then
+                call this%table(i)%thenode%release()
+                deallocate(this%table(i)%thenode)
+            end if
+        end do
+        ! remove the table itself
+        deallocate (this%table)
+    end subroutine
+
+    ! procedures of the nodepointer -------------------------------------------
+
 
     ! procedures of the node --------------------------------------------------
 
     ! the constructor
     function constructor_node(key, value, copy)
         class(node), pointer :: constructor_node
-        class(*), target :: key
+        class(*), intent(in) :: key
         class(*), target :: value
         logical, optional :: copy
-        class(*), pointer :: test
 
         ! allocate the memory for the new node
         allocate(constructor_node)
@@ -155,5 +282,22 @@ contains
         ! calculate the hash code
         constructor_node%hash = calculateHash(key)
     end function
+
+    ! free the memory allocated inside of this node
+    subroutine release(this)
+        class(node) :: this
+        ! clean the node itself
+        if (this%valueIsCopy) then
+            deallocate(this%value)
+        else
+            nullify(this%value)
+        end if
+        deallocate(this%key)
+        ! clean children of this node
+        if (associated(this%next)) then
+            call this%next%release()
+            deallocate(this%next)
+        end if
+    end subroutine
 
 end module mod_map
--- testsrc/test-map.f90
+++ testsrc/test-map.f90
@@ -6,21 +6,55 @@ program test_map
     class(map), pointer :: testmap
     character (len=50), allocatable :: key, value
     real (kind=8) :: k2, v2
+    integer :: i
 
     allocate(key)
     allocate(value)
 
     ! create the map
-    testmap => map()
+    testmap => map(7)
 
     ! create key and value
     key = "the key"
     value = "the value"
 
     ! add is to the map
-    k2 = 1.1
+    call testmap%printContent()
     v2 = 2.2
+    k2 = 1.1
     call testmap%add(k2, v2, .true.)
-    call testmap%add(key, value, .true.)
+    call testmap%printContent()
+
+    k2 = 0.1
+    call testmap%add(k2, v2, .true.)
+    call testmap%printContent()
+
+    k2 = 1.1
+    call testmap%add(k2, v2, .true.)
+    call testmap%printContent()
+
+    k2 = 0.2
+    call testmap%add(k2, v2, .true.)
+    call testmap%printContent()
+
+    k2 = 0.1
+    call testmap%add(k2, v2, .true.)
+    call testmap%printContent()
+
+    k2 = -1.3
+    call testmap%add(k2, v2, .true.)
+    call testmap%printContent()
+
+    ! any memory holes?
+    do i = 1, 10000
+        k2 = 0.1
+        call testmap%add(k2, v2, .true.)
+    end do
+
+    ! clean up
+    call testmap%printContent()
+    call testmap%removeAll()
+    call testmap%printContent()
+
 
 end program test_map
